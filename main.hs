605d1397251e973e9e9e6a7ddfc4827b7604fd6e
HAZELNUT <stran19@student.aau.dk>
2023-09-22 16:25:25 +0200

    Started work on some instructions

--- main.hs
+++ main.hs
@@ -4,10 +4,11 @@ import Text.Printf (printf)
 import System.IO (openFile)
 import GHC.IO.IOMode (IOMode(ReadMode))
 import GHC.IO.Handle (hFileSize)
-import Data.ByteString as BS (ByteString, hGet, tail, head, take, index, empty, uncons, length)
+import Data.ByteString as BS (ByteString, hGet, tail, head, take, index, empty, uncons, length, singleton)
 import Data.Binary (Word8, Word16)
 import Data.Map (Map, fromList, (!?), size)
 import Data.Char (intToDigit)
+import Data.Bits
 
 type Opcode = String
 type Byte = Word8
@@ -26,10 +27,10 @@ data CCState = CCState {
   si :: Word8,
   z  :: Word8,
   p  :: Word8
-}
+} deriving Show
 
 data State8080 = State8080 {
-  a      :: Word8,
+  a      :: Word16,
   b      :: Word8,
   c      :: Word8,
   d      :: Word8,
@@ -41,7 +42,7 @@ data State8080 = State8080 {
   mem    :: ByteString,
   ccodes :: CCState,
   inte   :: Word8
-}
+} deriving Show
 
 
 i2ToString :: ByteString -> Int -> String
@@ -337,21 +338,34 @@ dissasemble = do f <- openFile "space-invaders.rom" ReadMode
                  return 0
 
 instructionNotImplemented :: State8080 -> a
-instructionNotImplemented state = error "Instruction not implemented"
+instructionNotImplemented state = error ("Instruction not implemented: " ++ show state {mem = BS.singleton (BS.index (mem state) (pc state))})
 
 emulateProgram :: State8080 -> State8080
-emulateProgram state = error "hej"--emulateProgram (emulateOp state)
+emulateProgram state | pc state < BS.length (mem state) = emulateProgram (emulateOp state)
+                     | otherwise = state
 
 emulateOp :: State8080 -> State8080
-emulateOp state | op == 0x00 = state
-                | op == 0x01 = loadPairImmediate 'b' state
+emulateOp state | op == 0x00 = state {pc = pc state + 1}
+                | op == 0x01 = loadPairImmediate 'B' state
+                | op == 0xc3 = jmp state
+                | otherwise = instructionNotImplemented state
           where op = BS.index (mem state) (pc state)
 
 loadPairImmediate :: Char -> State8080 -> State8080
-loadPairImmediate 'b' state = state {b = BS.index (mem state) (pc state + 2),
+loadPairImmediate 'B' state = state {b = BS.index (mem state) (pc state + 2),
                                      c = BS.index (mem state) (pc state + 1),
                                      pc = pc state + 3}
 
+jmp :: State8080 -> State8080
+jmp state = state {pc = fromIntegral adr}
+      where adr = b `shiftL` 8 .|. a
+            a = fromIntegral (getNNextByte state 1) :: Word16
+            b = fromIntegral (getNNextByte state 2) :: Word16
+
+getNNextByte :: State8080 -> Int -> Word8
+getNNextByte state n = BS.index (mem state) (pc state + n)
+
+main :: IO State8080
 main = do f <- openFile "space-invaders.rom" ReadMode
           size <- hFileSize f
           buffer <- hGet f (fromIntegral size)
